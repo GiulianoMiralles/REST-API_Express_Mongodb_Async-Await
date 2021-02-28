9319bb3db60474cb9cef059a96a5d2a67ced1b18
G-Miralles <g.miralles@itecriocuarto.org.ar>
2021-02-28 16:53:56 -0300

    connection to the mongoDB database

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb://localhost/REST-API_Express_Mongodb_Async-Await',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(db => console.log('db is connected'))
+    .catch(err => console.log(err));
 
 //settings
 app.set('port', process.env.PORT || 3000); //verifica en mi sistema operativo si hay algun recurso que ya ste usando el puerto 3000 si no es asi lo ocupara mi aplicacion
 
 //middleware
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+app.use(morgan('dev'));                 //Me permite ver por consolas todas las peticiones http que hago en mi servidor
+app.use(bodyParser.json());             //Me permite leer los formatos json que llegan a mi servidor
 
 
 //Start the server
